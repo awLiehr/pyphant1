39bc7a4eaf022e871264617fe2e24824d5e91f9b
Michael Zhang <mzha612@aucklanduni.ac.nz>
2019-02-15 12:02:27 +1300

    Fix ModuleNotFoundError

--- src/pyphant/pyphant/quantities/ParseQuantities.py
+++ src/pyphant/pyphant/quantities/ParseQuantities.py
@@ -30,13 +30,13 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-u"""
+"""
 """
 
 
 import mx.DateTime.ISO
-from pyphant.quantities import Quantity
-from pyphant.quantities.PhysicalQuantities import PhysicalQuantity
+from src.pyphant.pyphant.quantities import Quantity
+from src.pyphant.pyphant.quantities.PhysicalQuantities import PhysicalQuantity
 
 import logging
 _logger = logging.getLogger("pyphant")
